72882aee2fad4fd4395f0b896c8a5f4757f1f737
Riyad Rahimoff <riyad_rahimov.dev@mail.ru>
2025-08-09 01:32:33 +0400

    added Swagger

--- src/main/java/com/azecoders/rrbank/config/SwaggerConfig.java
+++ src/main/java/com/azecoders/rrbank/config/SwaggerConfig.java
@@ -1,13 +1,8 @@
-package com.azecoders.community.config;
+package com.azecoders.rrbank.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @SecurityScheme(
