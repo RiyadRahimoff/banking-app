e48f6c3766e3fb34c42eb6413b7fc86e52d1b309
Riyad Rahimoff <riyad_rahimov.dev@mail.ru>
2025-08-09 01:33:29 +0400

    added security

--- src/main/java/com/azecoders/rrbank/security/SecurityConfig.java
+++ src/main/java/com/azecoders/rrbank/security/SecurityConfig.java
@@ -3,14 +3,38 @@ package com.azecoders.rrbank.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/api/register",
+                                "/api/verify",
+                                "/api/login",
+                                "/api/refresh-token",
+                                "/api/logout",
+                                "/user/complete",
+                                "/api/enums/professions",
+                                "/api/v1/files/upload"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
