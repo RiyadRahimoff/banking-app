b6313d1e4eedc112c015ffc23b80d980d6193edb
Riyad Rahimoff <riyad_rahimov.dev@mail.ru>
2025-08-09 01:34:37 +0400

    added mail system

--- src/main/java/com/azecoders/rrbank/service/concrete/MailServiceHandler.java
+++ src/main/java/com/azecoders/rrbank/service/concrete/MailServiceHandler.java
@@ -1,4 +1,45 @@
 package com.azecoders.rrbank.service.concrete;
 
-public class MailServiceHandler {
+import com.azecoders.rrbank.service.abstraction.MailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailServiceHandler implements MailService {
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void sendVerificationCode(String to, String code, String fullName) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("RR bank account confirmation code");
+        message.setText("Hello " + fullName + "\n\n" + "Your confirmation code: " + code);
+
+        try {
+            mailSender.send(message);
+
+        } catch (Exception ex) {
+            throw new MailSendException("Mail system not working!");
+        }
+    }
+
+    @Override
+    public void sendAccountConfirmedMessage(String to, String fullName) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setText("Dear "+fullName+" your account confirmed successfully.Please submit your other information");
+        try {
+            mailSender.send(message);
+        }catch (Exception exception){
+            throw new MailSendException("Mail system not working!");
+        }
+    }
 }
