521c889af674ecb961e09cc8f132695b9480f00d
RiyadRahimoff <riyad_rahimov.dev@mail.ru>
2025-08-12 10:16:44 +0400

    Access,refresh token generate system

---
+++ src/main/java/com/azecoders/rrbank/service/concrete/JwtService.java
@@ -0,0 +1,71 @@
+package com.azecoders.rrbank.service.concrete;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Service
+@FieldDefaults(level = PRIVATE)
+public class JwtService {
+    @Value("${jwt.secret}")
+    String secretKey;
+    @Value("${jwt.access-expiration}")
+    long accessExpiration;
+    @Value("${jwt.access-expiration}")
+    long refreshExpiration;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(Long id) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(id))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(Long id) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(id))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException ex) {
+            return false;
+        }
+    }
+
+
+}
