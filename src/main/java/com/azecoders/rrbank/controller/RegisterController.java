9306b2aef8dad63c40a49764b943ddfc382a1e11
RiyadRahimoff <riyad_rahimov.dev@mail.ru>
2025-08-13 09:57:03 +0400

    reformat code

--- src/main/java/com/azecoders/rrbank/controller/RegisterController.java
+++ src/main/java/com/azecoders/rrbank/controller/RegisterController.java
@@ -5,27 +5,23 @@ import com.azecoders.rrbank.model.response.RegisterResponse;
 import com.azecoders.rrbank.service.concrete.RegisterServiceHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class RegisterController{
+public class RegisterController {
     private final RegisterServiceHandler registerService;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.OK)
-    public RegisterResponse registerUser(CreateRegisterRequest registerRequest) {
+    public RegisterResponse registerUser(@RequestBody CreateRegisterRequest registerRequest) {
         return registerService.registerUser(registerRequest);
     }
 
     @PostMapping("/verify")
     @ResponseStatus(HttpStatus.OK)
-    public String verifyuser(String otpCode) {
-        return registerService.verifyuser(otpCode);
+    public String verifyUser(@RequestParam String otpCode) {
+        return registerService.verifyUser(otpCode);
     }
-
 }
